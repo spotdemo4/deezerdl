8bd1f92c1d33b84a62d9c3a8c360e93cc6b77094
Trevor <trevor@quanta-inc.com>
2023-11-29 14:21:03 -0500

    feat: download from youtube playlist

--- Dockerfile
+++ Dockerfile
@@ -6,12 +6,14 @@ RUN python3 -m venv /opt/venv
 ENV PATH="/opt/venv/bin:$PATH"
 
 # Get dependencies
+RUN apt update && apt install -y ffmpeg
 RUN git clone https://github.com/yarrm80s/orpheusdl.git
 RUN pip install -r orpheusdl/requirements.txt
 RUN git clone https://github.com/uhwot/orpheusdl-deezer orpheusdl/modules/deezer
 COPY settings.json /app/orpheusdl/config/settings.json
 RUN pip install deezer-python
 RUN pip install python-dotenv
+RUN pip install yt-dlp
 
 # Run
 COPY . /app
--- autodownload.py
+++ autodownload.py
@@ -2,6 +2,8 @@ import subprocess
 import deezer
 import time
 import os
+import json
+import glob
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -11,7 +13,8 @@ while True:
     print("Logging in...")
     client = deezer.Client(access_token=os.environ.get("ACCESS_TOKEN"))
 
-    print("Getting playlist...")
+    # Deezer download
+    print("Getting deezer playlist...")
     playlist_tracks = client.get_playlist(int(os.environ.get("PLAYLIST_ID"))).tracks
     for track in playlist_tracks:
         print("Downloading " + track.artist.name + ", " + track.title + "...")
@@ -20,5 +23,39 @@ while True:
         print("Deleting " + track.artist.name + ", " + track.title + "...")
         client.get_playlist(int(os.environ.get("PLAYLIST_ID"))).delete_tracks([track])
     
+    # Youtube download
+    print("Getting youtube playlist...")
+    yt_json = json.loads(subprocess.run(["yt-dlp", "-J", "-s", "-q", "https://www.youtube.com/playlist?list=" + os.environ.get("YT_PLAYLIST_ID")], capture_output=True).stdout.decode("utf-8"))
+    for video in yt_json["entries"]:
+        # If the video isn't a music video
+        if (video["album"] == None or video["artist"] == None or video["track"] == None):
+            # If the video has already been downloaded, skip it
+            if (glob.glob(os.environ.get("DL_LOCATION") + "/" + video["channel"] + "/" + video["title"] + ".*")):
+                continue
+
+            # Download the video
+            print("Downloading " + video["channel"] + ", " + video["title"] + "...")
+            subprocess.run(["yt-dlp", "-x", "-P", os.environ.get("DL_LOCATION"), "-o", "%(channel)s/%(title)s.%(ext)s", video["webpage_url"]])
+        
+        # If the video is a music video
+        else:
+            # If the video is already on deezer, download it from there
+            search = client.search(artist=video["artist"], album=video["album"], track=video["track"])
+            if len(search) == 0:
+                # If the video has already been downloaded, skip it
+                if (glob.glob(os.environ.get("DL_LOCATION") + "/" + video["artist"] + "/" + video["album"] + "/" + video["track"] + ".*")):
+                    continue
+
+                print("Downloading " + video["artist"] + ", " + video["track"] + "...")
+                subprocess.run(["yt-dlp", "-x", "-P", os.environ.get("DL_LOCATION"), "-o", "%(artist)s/%(album)s/%(track)s.%(ext)s", video["webpage_url"]])
+            else:
+                # If the video has already been downloaded, skip it
+                if (glob.glob(os.environ.get("DL_LOCATION") + "/" + search[0].artist.name + "/" + search[0].album.title + "/" + search[0].title + ".*")):
+                    continue
+                
+                print("Downloading " + search[0].artist.name + ", " + search[0].title + "...")
+                subprocess.run(["python", "orpheus.py", "--output", os.environ.get("DL_LOCATION") + "/" + search[0].artist.name + "/" + search[0].album.title + "/", search[0].link], cwd="orpheusdl/")
+
     print("Sleeping for 1 hour...")
+    print("")
     time.sleep(3600)
